de9c7640cc6936a68dc3b37f2458396b2aa70f8f
Zachary Boyd <zachary@zacharyboyd.nyc>
2019-08-10 11:38:38 -0400

    Better sources, pause play

--- src/js/app.js
+++ src/js/app.js
@@ -3,11 +3,6 @@ require('velocity-animate');
 const ejs = require('ejs');
 const Reveal = window.Reveal = require('reveal.js/js/reveal');
 
-/* Taken from <https://stackoverflow.com/a/5624139>. Thank you Tim Down! */
-function rgbToHex(r, g, b) {
-    return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
-}
-
 (async () => {
     const fns = {
 
@@ -15,12 +10,51 @@ function rgbToHex(r, g, b) {
 
     const existingMaps = {};
     Reveal.addEventListener('slidechanged', (event) => {
-        if ($(event.currentSlide).is('.full')) {
+        const ele = $(event.currentSlide);
+        if ($(ele).is('.full')) {
             $('.slides').addClass('full');
         } else {
             $('.slides').removeClass('full');
         }
-    });
+
+        setTimeout(() => {
+            const video = $('.slide-background.present video')[0];
+            if (video) {
+                $(video).css({ opacity: '1' });
+                $(ele).addClass('playing');
+                video.onpause = void(0);
+                video.onpause = () => {
+                    $(ele).removeClass('playing');
+                };
+
+                video.onplay = () => {
+                    $(ele).addClass('playing');
+                };
+
+                function enter () {
+                    video.pause();
+                    $(video).animate({ opacity: '0.75' });
+                }; 
+
+                function leave() {
+                    video.play();
+                    $(video).animate({ opacity: '1' });
+                }; 
+                
+                $('p', ele).on('mouseenter', enter);
+                $('p', ele).on('mouseleave', leave);
+
+                video.onended = () => {
+                    $(ele).removeClass('playing');
+                    $(video).animate({ opacity: '1' });
+                    video.onpause = void(0);
+                    video.onplay = void(0);
+                    $('p', ele).off('mouseenter', enter);
+                    $('p', ele).off('mouseleave', leave);
+                };
+            }
+        }, 4);
+     });
     
     Reveal.initialize({
         autoPlayMedia: true,
