0b320f6d0a6ae6b38d83310504a14698db03aa26
Zachary Boyd <zachary@zacharyboyd.nyc>
2019-08-10 11:41:37 -0400

    Better sources, pause play

--- build-data.js
+++ build-data.js
@@ -1,8 +1,3 @@
-/*
-    Results by district obtained from http://bit.ly/2Km68Qf - © 2012 Kos Media, LLC
-    Results by state obtained from http://bit.ly/2YKQSWg - © 2017 MIT Election Data and Science Lab
-    Economic and Population data obtained from https://www.census.gov/mycd - The United States Census Bureau
-*/
 const path = require('path');
 const shell = require('shelljs');
 
